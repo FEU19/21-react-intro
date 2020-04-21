d8659fafa755c714772e11a91b3d296a201c1996
David Andersson <lejonmanen@gmail.com>
2020-04-21 11:51:19 +0200

    State and props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Space from './components/Space';
 
 function App() {
+    // State-variabler överst
     const price = 2000000;
+    const [selectedPlanet, setPlanet] = useState('Pluto');
+
+    // Eventuella funktion
+    const setPlanetJupiter = () => setPlanet('Jupiter');
+
+    // Returnera JSX
     return (
         <div className="App">
             <header className="App-header">
                 <h1>React in Space</h1>
-                <Space planet="Jupiter" cost={price} />
             </header>
+            <main>
+                <h2>Where do you want to go?</h2>
+                <div>
+                    <button onClick={setPlanetJupiter}> Jupiter </button>
+                    <button onClick={() => setPlanet('Mars')}> Mars </button>
+                    <button onClick={() => setPlanet('Pluto')}> Pluto </button>
+                </div>
+                <Space planet={selectedPlanet} cost={price} />
+            </main>
         </div>
     );
 }
-// Vue:       :v-bind="'Jupiter'"
+// React:     planet={'Jupiter'}
+// Vue:       :planet="'Jupiter'"
 // Angular:   [planet]="'Jupiter'"
 
 export default App;
--- src/components/Space.jsx
+++ src/components/Space.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 // props == { planet: 'Jupiter', cost: '2000000' }
-const Space = (props) => (
-    <div>Space! Let's go to {props.planet}, it only costs {props.cost}</div>
+const Space = ({ planet, cost }) => (
+    <div>Space! Let's go to {planet}, it only costs {cost}</div>
 )
 
 export default Space;
